e2b06e5f262f5fb1022bb2d8d2ce5569b5770ae4
Matías Aguirre <matiasaguirre@gmail.com>
2015-02-26 00:57:34 -0200

    Remove unused functions

--- m2x/utils.py
+++ m2x/utils.py
@@ -1,7 +1,6 @@
 import time
 
 from datetime import date, datetime
-from functools import wraps
 from json import JSONEncoder
 
 from iso8601 import iso8601
@@ -13,44 +12,6 @@ class DateTimeJSONEncoder(JSONEncoder):
             return to_iso(value)
         else:
             return super(DateTimeJSONEncoder, self).default(value)
-
-
-def memoize(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        name = '_{0}'.format(func.__name__)
-        if not hasattr(self, name):
-            result = func(self, *args, **kwargs)
-            setattr(self, name, result)
-            return result
-        return getattr(self, name)
-    return wrapper
-
-
-def pmemoize(func):
-    return property(memoize(func))
-
-
-def process_values(values, timestamp_name='at'):
-    return list(map(lambda v: process_values(v, timestamp_name), values))
-
-
-def process_value(value, timestamp_name='at'):
-    if isinstance(value, tuple):
-        if len(value) == 2:
-            value = {timestamp_name: value[0], 'value': value[1]}
-        elif len(value) == 1:
-            value = {'value': value[0]}
-    elif not isinstance(value, dict):
-        value = {'value': value}
-
-    # Ensure a datetime in the value, the server will ensure local
-    # datetime if no value is passed anyway, but since the server
-    # doesn't return the value created, there's no way to get it unless
-    # all the values are requested again
-    value[timestamp_name] = to_iso(value.get(timestamp_name) or
-                                   to_utc(datetime.now()))
-    return value
 
 
 def to_utc(dtime):
